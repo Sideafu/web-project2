c5eb5bb8a0656248318d07e8a2612887aa6617ee
Sideafu <100576790+Sideafu@users.noreply.github.com>
2023-02-06 19:51:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def main.py():
-  return render_template("html.html", text="hello world!")
+  return render_template("index.html", text="hello world!")
 
 app.run()
