274ae307b9b7627bc65e0249d69fdf43dff3715f
Sideafu <zhaoyifan4244@126.com>
2023-02-06 20:39:58 +0800

    修正错误

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import flask
 app = flask.Flask(__name__)
 
+
 @app.route("/")
-def main.py():
-  return render_template("index.html", text="hello world!")
+def main():
+    return flask.render_template("index.html", text="hello world!")
+
 
 app.run()
